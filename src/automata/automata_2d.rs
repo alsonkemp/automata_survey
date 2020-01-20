f20d35f0ba37bd5aa2bbaf1193cd9a519ff70d8a
Alson Kemp <alson@alsonkemp.com>
2020-01-19 23:30:50 -0800

    Adding 2D.  Now seriously need IsInterestingSimulation impl...
    Running this will result in lots of output files (most !interesting).

--- src/automata/automata_2d.rs
+++ src/automata/automata_2d.rs
@@ -1,42 +1,59 @@
-/* 1D first...
 use crate::automata::types::*;
-use ndarray::prelude::*;
+use ndarray::*;
 use rand::prelude::random;
 
-fn new(neighbors: u8, rules: &'static Rules, space_size: usize, max_iterations: usize) -> &Simulation {
-  &Simulation {
+fn conways_rules() -> Array1<u8> {
+  // len 2^9 for neighbors == 1
+  let mut rules = Array1::<u8>::zeros(Ix1(512));
+  for (i, e) in rules.indexed_iter_mut() {
+    let alive = i & 16 > 1;
+    let cnt = i.count_ones();
+    // Remember: cnt includes the *alive* cell so alive || (2+1) or (3+1)
+    if (!alive && cnt == 3) || (alive && (cnt == 3 || cnt == 4)) {
+      *e = 1;
+    }
+  };
+  rules
+}
+fn new_with_rules<'a>(neighbors: u32, space_size: u32,
+                             max_iterations: u32, rules : Array1<u8>) -> Simulation {
+  return Simulation {
     iteration: 0,
     max_iterations: max_iterations,
     automata: Automata {
       dimension: 2,
       neighbors,
-      rules: &rules,
+      rules: rules
     },
-    space: ArrayD::<u8>::zeros(IxDyn(&[max_iterations, space_size, space_size]))
-  }
+    space: ArrayD::<u8>::zeros(IxDyn(&[max_iterations as usize,
+                                       space_size as usize,
+                                       space_size as usize])).clone()
+  };
 }
 
-fn new_with_random_rules(neighbors: u8, space_size: usize, max_iterations: usize) -> &'static Simulation {
-  let randoms: Vec<u8> = (0..(2u32.pow(1 + 2 * neighbors as u32)))
-    .map(|_| random::<u8>())
-    .collect();
-  let rules = Array1::<u8>::from(randoms);
-  &Simulation {
-    iteration: 0,
-    max_iterations: max_iterations,
-    automata: Automata {
-      dimension: 1,
-      neighbors,
-      rules: &rules,
-    },
-    space: ArrayD::<u8>::zeros(IxDyn(&[max_iterations, space_size, space_size]))
-  }
+fn new_with_random_rules<'a>(neighbors: u32, space_size: u32,
+                             max_iterations: u32) -> Simulation {
+  let randoms = (0..512)
+    .map(|_| if random::<u8>() < 64 { 1 } else { 0 })
+    .collect::<Array1<u8>>();
+  new_with_rules(neighbors, space_size, max_iterations, randoms)
 }
 
-fn run_random_automata() {
-  let mut s : &Simulation = new_with_random_rules(1, 100, 100);
-  s.randomize_space();
-  println!("Rules = {}", s.automata.rules.to_str());
-  s.write_image();
+pub fn run_random_automata(n: u32) {
+  /*let _rules = conways_rules();
+  let mut _s = new_with_rules(n, 100, 100, _rules);
+  println!("Rules (Conway) = {}", _s.automata.rules.to_str(false));
+  println!("Interesting (Conway): {:?}", _s.automata.rules.get_interesting());
+  _s.randomize_space();
+  _s.iterate_all();
+  _s.write_image();
+  */
+  let mut s = new_with_random_rules(n, 100, 100);
+  if !s.path_exists() {
+    s.randomize_space();
+    println!("Rules (Random) = {}", s.automata.rules.to_str(false));
+    println!("Interesting (Random): {:?}", s.automata.rules.get_interesting());
+    s.iterate_all();
+    s.write_image();
+  }
 }
-*/
--- src/automata/types.rs
+++ src/automata/types.rs
@@ -32,28 +32,24 @@ pub trait TRules {
 }
 impl TRules for Rules {
   fn to_str(&self, bin: bool) -> String {
-    let mut v = Vec::<u8>::new();
     let mut acc = 0u8;
     let mut s = String::from("");
     for (idx, e) in self.indexed_iter() {
       let bit_idx = idx % 8;
       acc += e << bit_idx;
       if idx > 0 && bit_idx == 7 {
-        v.push(acc);
-        // println!("!!!!acc: {} {} {} {} {:?}", e, idx, bit_idx, acc, v);
+        if bin {
+          s.push_str(&format!("{:08b}", acc));
+        } else {
+          s.push_str(&format!("{:x}", acc));
+        }
+        // println!("!!!!acc: {} {} {} {} {:?}", e, idx, bit_idx, acc, s);
         acc = 0;
       } else {
         //println!("acc: {} {} {} {} {:?}", e, idx, bit_idx, acc, v);
       }
     }
-    for e in &v {
-      if bin {
-        s.push_str(&format!("{:08b}", e));
-      } else {
-        s.push_str(&format!("{:08}", e));
-      }
-    }
-    format!("{}", s)
+    s
   }
 }
 
@@ -62,25 +58,30 @@ impl TRules for Rules {
 #[derive(Debug)]
 pub struct InterestingRules {
   alive_ratio: f32,
+  is_interesting: bool,
 }
 impl fmt::Display for InterestingRules {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    if !self.is_interesting {
+      write!(f, "NotInteresting:").unwrap();
+    }
     write!(f, "{}", self.alive_ratio)
   }
 }
 // Trait for producing Interesting structs about Rules.
-trait IsInterestingRules {
+pub trait IsInterestingRules {
   fn get_interesting(&self) -> InterestingRules;
 }
-
 impl IsInterestingRules for Rules {
   fn get_interesting(&self) -> InterestingRules {
     let alive = self.iter().filter(|e| **e > 0).count() as f32;
     let total = self.len() as f32;
+    let a_r = alive / total;
     /* println!("alive, total, alive_ratio: {:?}, {}, {}, {}",
     self, alive, total, alive/total);*/
     InterestingRules {
-      alive_ratio: alive / total,
+      alive_ratio: a_r,
+      is_interesting: a_r > 0.2 && a_r < 0.3,
     }
   }
 }
@@ -122,34 +123,72 @@ impl Simulation {
   }
   pub fn path(&self) -> PathBuf {
     return Path::new(&format!(
-      "./output/{}/{}/{}:IR:{}.gif",
+      "./output/{}/{}/{}:{}.gif",
       self.automata.dimension,
       self.automata.neighbors,
-      self.automata.rules.to_str(true),
-      self.automata.rules.get_interesting()
+      self.automata.rules.get_interesting(),
+      self.automata.rules.to_str(false)
     ))
     .to_path_buf();
   }
+  fn iterate_2d(&mut self) {
+    // memos
+    let dimx = self.space.dim()[1];
+    let dimy = self.space.dim()[2];
+    let it = self.iteration as usize;
+    // Only for: dim1 == dim2
+    for x in 0..dimx {
+      let xdim = x + dimx;
+      for y in 0..dimy {
+        let ydim = y + dimy;
+        // TODO: Handle nei>1.  My brain is not large enough.
+        // Start accumulation
+        // Unrolled...
+        // Top-left
+        let mut acc = (self.space[[it, (xdim - 1) % dimx, (ydim - 1) % dimy]] as usize) << 0;
+        // Top-middle
+        acc += (self.space[[it, (xdim) % dimx, (ydim - 1) % dimy]] as usize) << 1;
+        // Top-right
+        acc += (self.space[[it, (xdim + 1) % dimx, (ydim - 1) % dimy]] as usize) << 2;
+        // Center-left
+        acc += (self.space[[it, (xdim - 1) % dimx, ydim % dimy]] as usize) << 3;
+        // Center-middle
+        acc += (self.space[[it, (xdim) % dimx, ydim % dimy]] as usize) << 4;
+        // Center-right
+        acc += (self.space[[it, (xdim + 1) % dimx, ydim % dimy]] as usize) << 5;
+        // Bottom-left
+        acc += (self.space[[it, (xdim - 1) % dimx, (ydim + 1) % dimy]] as usize) << 6;
+        // Bottom-middle
+        acc += (self.space[[it, (xdim) % dimx, (ydim + 1) % dimy]] as usize) << 7;
+        // Bottom-right
+        acc += (self.space[[it, (xdim + 1) % dimx, (ydim + 1) % dimy]] as usize) << 8;
+        let v = self.automata.rules[[acc]];
+        // set the value in the *next* plane to the decoded value
+        self.space[[1 + it, x, y]] = v;
+      }
+    }
+    self.iteration += 1;
+  }
   fn iterate_1d(&mut self) {
     let dim0 = self.space.dim()[0] as u32;
+    // memos
+    let nei = self.automata.neighbors as u32;
     for i in 0..dim0 {
-      // memos
-      let nei = self.automata.neighbors as u32;
       let idim0 = i + dim0;
       // Start accumulation with 2**neighbors
-      let mut acc: usize = self.space[[self.iteration as usize, i as usize]] as usize
-                            * 2usize.pow(nei);
+      let mut acc: usize =
+        self.space[[self.iteration as usize, i as usize]] as usize * 2usize.pow(nei);
       let _ac = self.space[[self.iteration as usize, i as usize]] as usize;
       for idx in 0..nei {
         let l = (idim0 - 1) % dim0;
         let r = (idim0 + 1) % dim0;
         // neighbors==1 && idx==0 -> 1; neighbors==2 && idx==0 -> 2; neighbors==2 && idx==1 -> 1
         acc += self.space[[self.iteration as usize, l as usize]] as usize
-                * 2usize.pow(nei + idx as u32 - 1u32);
+          * 2usize.pow(nei + idx as u32 - 1u32);
         // neighbors==1 && idx==0 -> 4; neighbors==2 && idx==0 -> 4; neighbors==2 && idx==1 -> 8
         acc += self.space[[self.iteration as usize, r as usize]] as usize
-                * 2usize.pow(idx as u32 + 2u32);
-         /*if self.iteration < 2 {
+          * 2usize.pow(idx as u32 + 2u32);
+        /*if self.iteration < 2 {
            print!("({},{},{}) {} {} {} -> {} ", l, i, r,
            self.space[[self.iteration as usize, l as usize]],
            _ac,
@@ -170,8 +209,14 @@ impl Simulation {
     self.iteration += 1;
   }
   pub fn iterate_all(&mut self) {
-    while !self.is_complete() {
-      self.iterate_1d();
+    if self.automata.rules.get_interesting().is_interesting {
+      while !self.is_complete() {
+        if self.space.ndim() == 2 {
+          self.iterate_1d();
+        } else {
+          self.iterate_2d();
+        }
+      }
     }
   }
   pub fn initialize_space(&mut self) {
@@ -201,7 +246,7 @@ impl Simulation {
     // 2D automata?
     if s.axes().count() == 3usize {
       // 2D
-      for x in 0..s.dim()[1] {
+      for x in 0..s.dim()[0] {
         for y in 0..s.dim()[1] {
           s[[0, x, y]] = random::<bool>() as u8;
         }
@@ -231,7 +276,8 @@ impl Simulation {
       sp.dim()[0] as u16,
       sp.dim()[1] as u16,
       color_map,
-    ).unwrap();
+    )
+    .unwrap();
     encoder.set(Repeat::Infinite).unwrap();
     // Handle 1D and 2D automata.  Default to 1D and
     // end after 1 frame.
@@ -245,21 +291,30 @@ impl Simulation {
       fw = sp.dim()[1] as u16;
       fh = sp.dim()[2] as u16;
     }
-    for state in 0..end {
-      println!("writing: {:?}\n", f);
+    println!("writing: {:?}\n", f);
+    if !self.automata.rules.get_interesting().is_interesting {
       let mut frame = Frame::default();
-      let mut sl = sp.view();
-      frame.width = fw;
-      frame.height = fh;
-      if end == 1 {
-        sl.slice_collapse(s![0.., 0..].as_ref());
-      } else {
-        sl.slice_collapse(s![state, 0.., 0..].as_ref());
-      }
-      let iter = sl.iter();
-      let arr = Array::from_iter(iter).map(|e| **e);
-      frame.buffer = Cow::Borrowed(arr.as_slice().unwrap());
+      frame.width = 2;
+      frame.height = 1;
+      frame.buffer = Cow::Borrowed(&[0, 1]);
       encoder.write_frame(&frame).unwrap();
+    // Auto-drop image file
+    } else {
+      for state in 0..end {
+        let mut frame = Frame::default();
+        let mut sl = sp.view();
+        frame.width = fw;
+        frame.height = fh;
+        if end == 1 {
+          sl.slice_collapse(s![0.., 0..].as_ref());
+        } else {
+          sl.slice_collapse(s![state, 0.., 0..].as_ref());
+        }
+        let iter = sl.iter();
+        let arr = Array::from_iter(iter).map(|e| **e);
+        frame.buffer = Cow::Borrowed(arr.as_slice().unwrap());
+        encoder.write_frame(&frame).unwrap();
+      }
     }
   }
 }
